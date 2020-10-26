66d6971b1e2ca0b9a45e83177f260e44b22952d1
Haley Huynh <haleyhtnt1122@gmail.com>
2020-10-26 03:19:42 -0700

    update

--- app/src/main/java/evilkingmedia/cueserve/com/evilkingmedia/Cartoon/CartoonActivityServer3.java
+++
@@ -1,6 +0,0 @@
-package evilkingmedia.cueserve.com.evilkingmedia.Cartoon;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class CartoonActivityServer3 extends AppCompatActivity {
-}
--- app/src/main/java/evilkingmedia/cueserve/com/evilkingmedia/Cartoon/CartoonActivityServer4.java
+++
@@ -1,6 +0,0 @@
-package evilkingmedia.cueserve.com.evilkingmedia.Cartoon;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class CartoonActivityServer4 extends AppCompatActivity {
-}
